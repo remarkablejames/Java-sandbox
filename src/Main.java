ebe000cedae4bf70eeb2a920ca401f0a07ab0c81
James Niyongira <lascasasjames85@gmail.com>
2023-05-11 08:50:04 -0400

    removed unnecessary comments

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-        
+
         for (int i = 1; i <= 5; i++) {
 
             System.out.println("i = " + i);
