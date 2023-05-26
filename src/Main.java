a59c673b9c9068e50a4537c83b576f59a063d120
James Niyongira <lascasasjames85@gmail.com>
2023-05-26 12:23:57 -0400

    explored Java Scanner class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        hello greetings = new hello();
-        greetings.sayHello();
+        int a,b,c;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        a = sc.nextInt();
+        System.out.println("Enter the second number: ");
+        b = sc.nextInt();
+        c = a + b;
+        System.out.println("The sum of the two numbers is: " + c);
+
     }
 }
 
